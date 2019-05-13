e62fe4dbbae9d86507a6b574b6f958bcfdd68c0c
Jose Varanam <jvaranam@gmail.com>
2019-05-13 17:23:54 +0530

    innitial

--- googleMap.ts
+++ googleMap.ts
@@ -289,4 +289,11 @@ export class GMapProvider {
       }
     }
   }
+  /*function arePointsNear(checkPoint, centerPoint, km) {
+  var ky = 40000 / 360;
+  var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
+  var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
+  var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
+  return Math.sqrt(dx * dx + dy * dy) <= km;
+}*/
 }
